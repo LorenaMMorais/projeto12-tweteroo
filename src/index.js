993083bd6bfcfe112b67bb70cd92d3806d2d9c07
lorenamm <lorenamorais@alunos.utfpr.edu.br>
2023-01-09 02:31:59 -0300

    POST: tweets

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ server.use(json());
 const PORT = 5000;
 
 const users = [];
+const tweets = [];
+
 server.post('/sign-up', (req, res) => {
     const body = req.body;
     const user = {
@@ -20,6 +22,16 @@ server.post('/sign-up', (req, res) => {
 
 server.get('/sign-up', (req,res) => {
     res.send(users);
+});
+
+server.post('/tweets', (req,res) => {
+    const body = req.body;
+    const tweet = {
+        username: body.username,
+        tweet: body.tweet
+    };
+    tweets.push(tweet);
+    res.send('OK');
 });
 
 server.listen(PORT,() =>{console.log('Server Rodando na porta', PORT)})
