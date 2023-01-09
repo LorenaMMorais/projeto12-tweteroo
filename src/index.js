f2af2d61539dc0df708fe559ea94cd79ae0e2309
lorenamm <lorenamorais@alunos.utfpr.edu.br>
2023-01-09 01:53:18 -0300

    POST(/sign-up): recebe username e avatar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-import express from "express";
+import express, { application, json } from "express";
+import cors from 'cors';
 
-const app = express();
-const PORT = 5050;
+const server = express();
+server.use(cors());
+server.use(json());
 
-app.listen(PORT,() =>{console.log('Server Rodando na porta', PORT)})
+const PORT = 5000;
+
+const users = [];
+server.post('/sign-up', (req, res) => {
+    const body = req.body;
+    const user = {
+        username: body.username,
+        avatar: body.avatar
+    };
+    users.push(user);
+    console.log('Foi!');
+    res.send('OK');
+});
+
+server.listen(PORT,() =>{console.log('Server Rodando na porta', PORT)})
